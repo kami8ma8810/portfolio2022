e4668bd6179b44aa4e4812f070eb1567e765380f
Hayato Kamiyama <kami8ma8810@gmail.com>
2022-04-27 13:49:08 +0900

    workページの画像マウス連動

--- src/assets/js/libs/parallax-mouse.js
+++ src/assets/js/libs/parallax-mouse.js
@@ -11,13 +11,19 @@ export default () => {
         force3D: true,
       });
       document.addEventListener('mousemove', (event) => {
-        let yPos = event.clientX / window.innerWidth - 0.5;
-        let xPos = event.clientY / window.innerHeight - 0.5;
+        // let xPos = event.clientY / window.innerHeight - 0.5;
+        let xPos = window.innerWidth - event.clientX * 2;
+        // let yPos = event.clientX / window.innerWidth - 0.5;
+        let yPos = window.innerHeight - event.clientY * 2;
 
         gsap.to(target, {
           transformOrigin: 'center center 50%',
-          rotateX: xPos * 80,
-          rotateY: yPos * -80,
+          // rotateX: xPos * 80,
+          // rotateY: yPos * -80,
+          rotateX: yPos / 30,
+          rotateY: -xPos / 24,
+          boxShadow: `${yPos} ${xPos} 4px rgba(0, 0, 0, 1)`,
+          // ease: 'power2.out',
         });
       });
     },
